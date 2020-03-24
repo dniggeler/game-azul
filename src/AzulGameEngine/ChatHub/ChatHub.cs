8a0f75f2374a4b505936d11c14a1a206b9d49ac9
Dieter Niggeler <dieter.niggeler@swisslife.ch>
2020-03-24 16:44:11 +0100

    SAVEPOINT

---
+++ src/AzulGameEngine/ChatHub/ChatHub.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using AzulGameEngine.ChatHub.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace AzulGameEngine.ChatHub
+{
+    public class ChatHub : Hub
+    {
+        private readonly ChatThread _chatThread;
+
+        public ChatHub(ChatThread chatThread)
+        {
+            _chatThread = chatThread;
+        }
+
+        public async Task SendMessage(string playerName, string message)
+        {
+            _chatThread.AddMessage(new ChatMessage
+            {
+                ClientId = 0,
+                Message = message,
+                PlayerName = playerName,
+                CreatedAt = DateTime.Now
+            });
+
+            await Clients.All.SendAsync("NewMessage", playerName, message);
+        }
+    }
+}
---
+++ src/AzulGameEngine/ChatHub/Models/ChatMessage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AzulGameEngine.ChatHub.Models
+{
+    public class ChatMessage
+    {
+        public int ClientId { get; set; }
+        public string PlayerName { get; set; }
+        public string Message { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
