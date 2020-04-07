f09e8fdf2b2e51d78d04495aff3baeffa184abcd
Dieter Niggeler <dieter.niggeler@swisslife.ch>
2020-04-07 20:38:01 +0200

    SAVEPOINT

---
+++ src/AzulGameEngine/Game/IGameState.cs
@@ -0,0 +1,9 @@
+﻿using LanguageExt;
+
+namespace AzulGameEngine.Game
+{
+    public interface IGameState
+    {
+        Either<string, (long GameId, long GameStateId)> Start();
+    }
+}
---
+++ src/AzulGameEngine/Game/InitialGameState.cs
@@ -0,0 +1,20 @@
+﻿using AzulGameEngine.Game.Models;
+using LanguageExt;
+
+namespace AzulGameEngine.Game
+{
+    public class InitialGameState : IGameState
+    {
+        private readonly GameEngine gameEngine;
+
+        public InitialGameState(GameEngine gameEngine)
+        {
+            this.gameEngine = gameEngine;
+        }
+
+        public Either<string, (long GameId, long GameStateId)> Start()
+        {
+            return this.gameEngine.Start();
+        }
+    }
+}
